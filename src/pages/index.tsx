e5483206cbcc7ef88d71999c916f89966d83b368
David Lamberson <lamberson.david.ray@gmail.com>
2023-10-08 20:41:02 -0400

    chore: cleanup stats

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,29 +3,30 @@ import { ChangeEvent, useCallback, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-type Stat = 'STR' | 'DEX' | 'CON' | 'INT' | 'WIS' | 'CHA'
+type Ability = 'STR' | 'DEX' | 'CON' | 'INT' | 'WIS' | 'CHA'
 type Stats = {
-  [key in Stat]: number;
-} & { left: number }
+  [key in Ability]: number;
+} & { left: number; }
 
-const abilities: Stat[] = ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA']
+const abilities: Ability[] = ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA']
+const baseStat = 9
+const maxPoints = 27
+const minStat = 8
+const maxStat = 17
 
 export default function Home() {
   const [stats, setStats] = useState<Stats>({
-    STR: 9,
-    DEX: 9,
-    CON: 9,
-    INT: 9,
-    WIS: 9,
-    CHA: 9,
-    left: 27,
+    STR: baseStat,
+    DEX: baseStat,
+    CON: baseStat,
+    INT: baseStat,
+    WIS: baseStat,
+    CHA: baseStat,
+    left: maxPoints,
   })
 
-  const handleStatChange = useCallback((stat: Stat) => ({ target: { value }}: ChangeEvent<HTMLInputElement>) => {
+  const handleStatChange = useCallback((stat: Ability) => ({ target: { value }}: ChangeEvent<HTMLInputElement>) => {
     const newValue = Math.floor(Number(value))
-
-    if (newValue < 8 || newValue > 17) return
-
     const multiplier = newValue > 15 ? 2 : 1
 
     setStats((prev) => {
@@ -36,15 +37,13 @@ export default function Home() {
       return {
         ...prev,
         [stat]: newValue,
-        left:remaining,
+        left: remaining,
       }
   })
   }, [])
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-6 lg:text-left">
         <p className="text-4xl font-bold lg:col-span-6 pb-2">Points Buy - {stats.left} points left</p>
         {abilities.map((ability) => (
@@ -54,6 +53,8 @@ export default function Home() {
               type="number"
               className="w-1/2 min-w-36 text-2xl font-bold text-left rounded-lg bg-transparent border border-solid pl-1"
               value={stats[ability]}
+              max={maxStat}
+              min={minStat}
               onChange={handleStatChange(ability)}
               />
           </div>
